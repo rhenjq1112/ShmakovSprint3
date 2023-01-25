3331f4f66eac4f98d572bb3b37d8ceae58145f52
rhenjq1112 <122388092+rhenjq1112@users.noreply.github.com>
2023-01-26 04:16:20 +0500

    Initial Commit

---
+++ ShmakovSprint3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShmakovSprint3
+//
+//  Created by 1 on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
